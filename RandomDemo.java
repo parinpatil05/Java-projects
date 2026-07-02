4bc4dfebaed978ff99386d504c6bfbb3e36c71d7
Parin Patil <parinvpatil@gmail.com>
2026-07-02 19:20:40 +0530

    Add RandomDemo class to generate random number and boolean

---
+++ RandomDemo.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        
+        int number;
+        boolean isHeads;
+
+        number = random.nextInt(1, 7);
+        
+        isHeads = random.nextBoolean();
+
+        System.out.println(number);
+        
+        if(isHeads){
+            System.out.println("HEADS");
+        }
+        else{
+            System.out.println("TAILS");
+        }
+    }
+}
